6b2cc79d35a7fcd04b3d96c01f3899ecc2d04e0d
liefswanson <liefs@sfu.ca>
2014-10-23 11:58:55 -0700

    changed naming convention for excercises from XX to exXX. eg: 01 is now ex01

---
+++ ex1/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  puts("Hello");
+  puts("This is another line");
+  puts("puts adds an implicite end of line... ");
+  puts("I didn't know that");
+  return 0;
+}
