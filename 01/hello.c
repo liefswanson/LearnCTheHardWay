30783b56620a2f691061b8a3a31f8bd9502e5a21
liefswanson <liefs@sfu.ca>
2014-10-23 11:56:29 -0700

    completed the bonus activities for ex1.

--- 01/hello.c
+++ 01/hello.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  puts("Hello, world");
-
+  puts("Hello");
+  puts("This is another line");
+  puts("puts adds an implicite end of line... ");
+  puts("I didn't know that");
   return 0;
 }
