070fe31d9cb22d1a011ef645e2c93c26bffc81e9
liefswanson <liefs@sfu.ca>
2014-10-23 12:02:03 -0700

    made a mistake in the last commit, and did not properly remove previous directories. That has been fixed.

--- 01/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-  puts("Hello");
-  puts("This is another line");
-  puts("puts adds an implicite end of line... ");
-  puts("I didn't know that");
-  return 0;
-}
